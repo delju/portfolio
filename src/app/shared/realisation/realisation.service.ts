6e58c64437216d204f3002edb41522cee5bb9015
delju <julene.delvaux@gmail.com>
2021-06-02 14:13:51 +0200

    change

--- src/app/shared/realisation/realisation.service.ts
+++ src/app/shared/realisation/realisation.service.ts
@@ -16,11 +16,11 @@ export class RealisationService {
   findAll(): Observable<HttpResponse<Realisation[]>>{
 
   const realisation =[
-  new Realisation (1, "Colonnes", "Réalisation de colonnes en plusieurs modules: div classique, Grid et Flexbox", "/assets/liens/Colonnes.pdf"),
-  new Realisation (2, "Health Food", "Réalisation d'un site statique sur le thème de la cuisine avec des animations sur liens", "/assets/liens/health-food.pdf"),
-  new Realisation (3, "WebDesign", "Réalisation d'un site statique sur les div", "/assets/liens/exerciceWebdesign.pdf"),
-  new Realisation (4, "Commentaires", "Réalisation d'un site statique personnel, en s'exerçant sur la rédaction de commentaires","/assets/liens/exercicecommentaire.pdf"),
-  new Realisation (5, "Exercice Portfolio", "Réalisation de site statique sur le thème du Portfolio", "/assets/liens/exerciceportfolio.pdf")
+  new Realisation (1, "Colonnes", "Réalisation de colonnes en plusieurs modules: div classique, Grid et Flexbox", "assets/liens/Colonnes.pdf"),
+  new Realisation (2, "Health Food", "Réalisation d'un site statique sur le thème de la cuisine avec des animations sur liens", "assets/liens/health-food.pdf"),
+  new Realisation (3, "WebDesign", "Réalisation d'un site statique sur les div", "assets/liens/exerciceWebdesign.pdf"),
+  new Realisation (4, "Commentaires", "Réalisation d'un site statique personnel, en s'exerçant sur la rédaction de commentaires","assets/liens/exercicecommentaire.pdf"),
+  new Realisation (5, "Exercice Portfolio", "Réalisation de site statique sur le thème du Portfolio", "assets/liens/exerciceportfolio.pdf")
 
 ];
 // Retourne un nouvel observable du tableau realisation dans body
--- src/app/shared/service/service.service.ts
+++ src/app/shared/service/service.service.ts
@@ -15,10 +15,10 @@ constructor( ) { }
 
 findAll(): Observable<HttpResponse<Ser[]>>{
 const ser = [
-new Ser (1, "HTML et CSS", "Création de site web statique: Langage HTML 5 et langage CSS3 ", "/assets/img/site%20web.png"),
-new Ser (2, "SQL", "Création et manipulation d'une base de données grâce au langage MySQL", "/assets/img/database.png"),
-new Ser (3, "Javascript", "Langage Javascript: manipulation du DOM", "/assets/img/javascript.png"),
-new Ser (4, "PHP", "Langage PHP: Création d'un CRUD et de formulaire", "/assets/img/php.png")
+new Ser (1, "HTML et CSS", "Création de site web statique: Langage HTML 5 et langage CSS3 ", "assets/img/site%20web.png"),
+new Ser (2, "SQL", "Création et manipulation d'une base de données grâce au langage MySQL", "assets/img/database.png"),
+new Ser (3, "Javascript", "Langage Javascript: manipulation du DOM", "assets/img/javascript.png"),
+new Ser (4, "PHP", "Langage PHP: Création d'un CRUD et de formulaire", "assets/img/php.png")
 ];
 // On retourne un nouvel observable de la constante ser dans le body
 return of(new HttpResponse({body:ser}));
